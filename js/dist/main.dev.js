6d7916e44f78147b51129788423cac277b454813
Damir <damir.dmr88@gmail.com>
2022-05-30 18:30:49 +0600

    amocrm form add

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -66,54 +66,7 @@ $(document).ready(function () {
     image: {
       verticalFit: true
     }
-  }); //E-mail Ajax Send
-
-  $("#form-kta").submit(function () {
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "thanks.php",
-      data: th.serialize()
-    }).done(function () {
-      // alert("Спасибо, ваша заявка принята!");
-      $.magnificPopup.open({
-        items: {
-          src: '#thanksPopup'
-        },
-        type: 'inline'
-      });
-      setTimeout(function () {
-        // Done Functions
-        // $.magnificPopup.close();
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  }); // window.onscroll = function() {fixMMenu()};
-  // var header = document.getElementById("headerTop");
-  // var menu = document.getElementById("mmWrap");
-  // var sticky = header.offsetTop;
-  // function fixMMenu() {
-  // 	if (window.pageYOffset > sticky) {
-  // 		menu.classList.add("fix");
-  // 	} else {
-  // 		menu.classList.remove("fix");
-  // 	}
-  // }
-  // if(window.matchMedia('(max-width: 540px)').matches){
-  // 	$('.slider-wrap').addClass('owl-carousel');
-  // 	$('.slider-wrap').owlCarousel({
-  // 		nav:true,
-  // 		navText: '',
-  // 		items:1
-  // 	});
-  // }
-  // $('#mmenu-popup .menu a').on('click', function(){
-  // 	$.magnificPopup.close();
-  // });
-  // const observer = lozad(); // lazy loads elements with default selector as '.lozad'
-  // observer.observe();
-  // Отрисовка ползунка в "ОНЛАЙН КАЛЬКУЛЯТОР"
+  }); // Отрисовка ползунка в "ОНЛАЙН КАЛЬКУЛЯТОР"
 
   var range = document.getElementById("kv-range"); // Отрисовка ползунка в "ОНЛАЙН КАЛЬКУЛЯТОР"
 
--- js/main.js
+++ js/main.js
@@ -72,64 +72,6 @@ $(document).ready(function () {
 
 	});
 
-	//E-mail Ajax Send
-	$("#form-kta").submit(function () {
-		var th = $(this);
-
-		$.ajax({
-			type: "POST",
-			url: "thanks.php",
-			data: th.serialize()
-		}).done(function () {
-			// alert("Спасибо, ваша заявка принята!");
-			$.magnificPopup.open({
-				items: {
-					src: '#thanksPopup'
-				},
-				type: 'inline'
-			});
-			setTimeout(function () {
-				// Done Functions
-				// $.magnificPopup.close();
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-
-	
-
-	// window.onscroll = function() {fixMMenu()};
-	// var header = document.getElementById("headerTop");
-	// var menu = document.getElementById("mmWrap");
-	// var sticky = header.offsetTop;
-	// function fixMMenu() {
-	// 	if (window.pageYOffset > sticky) {
-	// 		menu.classList.add("fix");
-	// 	} else {
-	// 		menu.classList.remove("fix");
-	// 	}
-	// }
-
-	// if(window.matchMedia('(max-width: 540px)').matches){
-
-	// 	$('.slider-wrap').addClass('owl-carousel');
-
-	// 	$('.slider-wrap').owlCarousel({
-	// 		nav:true,
-	// 		navText: '',
-	// 		items:1
-	// 	});
-	// }
-	// $('#mmenu-popup .menu a').on('click', function(){
-	// 	$.magnificPopup.close();
-	// });
-
-	// const observer = lozad(); // lazy loads elements with default selector as '.lozad'
-	// observer.observe();
-
-
-
 	// Отрисовка ползунка в "ОНЛАЙН КАЛЬКУЛЯТОР"
 	const range = document.getElementById("kv-range");
 	// Отрисовка ползунка в "ОНЛАЙН КАЛЬКУЛЯТОР"
@@ -157,10 +99,6 @@ $(document).ready(function () {
 		$('.kv-range-gradient')[0].style.background = `linear-gradient(90deg, #1A1F26 ${left * 0.5 + 2}%, rgba(26, 31, 38, 0.23) ${left * 0.5 + 10}%)`;
 
 	});
-
-
-
-
 
 	// Отрисовка результата в "ОНЛАЙН КАЛЬКУЛЯТОР"
 	const calculate = {
